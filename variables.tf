4345bf1c73a2a53100a8beab7d7b82c02339ac2f
jakebark <jacob.barker@gmail.com>
2025-06-10 21:44:25 +0100

    vars desc

--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable "name" {
-  type    = string
-  default = null
+  description = "bucket name"
+  type        = string
+  default     = null
 }
 
 variable "kms_key" {
-  type    = string
-  default = null
+  description = "AWS KMS key arn"
+  type        = string
+  default     = null
 }
 
 variable "access_logging_bucket" {
-  type    = string
-  default = null
+  description = "s3 server access logging bucket name"
+  type        = string
+  default     = null
 }
 
 variable "ddb_table" {
-  type    = bool
-  default = false
+  description = "enable DynamoDB table"
+  type        = bool
+  default     = false
 }
