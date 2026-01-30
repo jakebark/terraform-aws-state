e489772fcf560516e35618a7c0e2bc88ede24602
jakebark <jacob.barker@gmail.com>
2026-01-30 20:36:03 +0000

    reorder var

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "name" {
-  description = "bucket name"
-  type        = string
-  default     = null
-}
-
-variable "kms_key" {
-  description = "AWS KMS key arn"
-  type        = string
-  default     = null
-}
-
 variable "access_logging_bucket" {
   description = "s3 server access logging bucket name"
   type        = string
@@ -27,3 +15,16 @@ variable "ddb_table" {
   type        = bool
   default     = false
 }
+
+variable "kms_key" {
+  description = "AWS KMS key arn"
+  type        = string
+  default     = null
+}
+
+variable "name" {
+  description = "bucket name"
+  type        = string
+  default     = null
+}
+
